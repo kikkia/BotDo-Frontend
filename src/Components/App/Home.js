cdd2d6c82e7883c22f7691e5c48bc001d5b42b88
kikkia <jesswalters53@gmail.com>
2021-06-17 12:50:29 -0500

    try to direct to login or dashboard based on auth

--- src/Components/App/Home.js
+++ src/Components/App/Home.js
@@ -1,12 +1,35 @@
 import React from 'react';
 
-const home = () => {
-    return (
-       <div>
-          <h1>Home</h1>
-           <p>Home page body content</p>
-       </div>
-    );
+class Home extends React.Component {
+    constructor(props, context) {
+        super(props)
+        this.amIAuthed();
+    }
+
+    amIAuthed() {
+        fetch("http://localhost:42069/auth/test", 
+        {
+            credentials: 'include',
+            mode: "cors"
+        })
+            .then(
+                (result) => {
+                    this.props.history.push('/dashboard');
+                },
+                (error) => {
+                    this.props.history.push('/login');
+                }
+            )
+    }
+    
+    render() {
+        return (
+        <div>
+            <h1>Home</h1>
+            <p>Home page body content</p>
+        </div>
+        );
+    }
 }
  
-export default home;
+export default Home;
